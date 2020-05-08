1474dec09d297af3fc7502e9fd28f5c7436a5d93
munchkinhalfling <munchkinhalfling@pm.me>
2020-05-08 15:26:32 -0400

    Add install button

---
+++ src/app.css-extensions.js
@@ -0,0 +1,6 @@
+export function hide(elem) {
+    elem.classList.add('hidden');
+}
+export function unhide(elem) {
+    elem.classList.remove('hidden');
+}
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
 import * as tacocat from './jspm_packages/npm/tacocat@1.0.0/tacocat.js';
+import storage from './jspm_packages/npm/kv-storage-polyfill@2.0.0/dist/kv-storage-polyfill.mjs';
 import DataRow from './dataRow.js';
-let installPrompt;
+import {hide, unhide} from './app.css.js';
 if('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js');
@@ -18,7 +19,21 @@ const reloadData = async () => {
         tacocat.render(new DataRow({apiResult: data[state], stateInfo, stateHistorical}), dataBody);
     }
 }
-
+window.addEventListener("beforeinstallprompt", async beforeInstallPromptEvent => {
+    beforeInstallPromptEvent.preventDefault(); // Prevents immediate prompt display
+  
+    // Shows prompt after a user clicks an "install" button
+    installButton.addEventListener("click", async function(mouseEvent) {
+      // you should not use the MouseEvent here, obviously
+      beforeInstallPromptEvent.prompt();
+      if(beforeInstallPromptEvent.userChoice == 'accepted') {
+          await storage.set('installed', true);
+      }
+    });
+  
+    if(!await storage.get('installed')) unhide(installButton); // Make button operable
+  });
+mdc.ripple.MDCRipple.attachTo(installButton);
 reloadDataBtn.addEventListener('click', reloadData);
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-fab'))
 reloadData();
